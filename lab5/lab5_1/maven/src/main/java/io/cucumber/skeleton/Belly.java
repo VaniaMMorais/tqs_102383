be5aedbfba02a90f7fba4a5fb7d1bc4ac43ce57b
Vania 102383 <vania.morais@ua.pt>
2023-04-13 18:18:34 +0100

    lab5

---
+++ lab5/lab5_1/maven/src/main/java/io/cucumber/skeleton/Belly.java
@@ -0,0 +1,9 @@
+package io.cucumber.skeleton;
+
+public class Belly {
+    public void eat(int cukes) {}
+    
+    public void wait(int time) {}
+
+    public void growl() {}
+}
---
+++ lab5/lab5_2/src/main/java/lab5/Book.java
@@ -0,0 +1,19 @@
+package main.java.lab5;
+
+import java.util.Date;
+
+public class Book {
+    private String title;
+	private String author;
+	private Date published;
+
+    public Book(String title, String author, Date date) {
+        this.title = title;
+        this.author = author;
+        this.published = date;
+    }
+
+    public String getTitle() {return this.title; }
+    public String getAuthor() {return this.author; }
+    public Date getPublished() {return this.published; }
+}
